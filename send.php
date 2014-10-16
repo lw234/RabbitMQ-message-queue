291eb51762f4fbb851f76449ad70d7b1199466a3
lw234 <lynn291991@yahoo.com>
2014-10-15 23:36:18 -0400

    update

--- send.php
+++ send.php
@@ -1,20 +1,30 @@
 <?php
-require_once ('vendor/autoload.php');
+
+// Include the autoload library and use the necessary classes
+
+require_once __DIR__ . '/vendor/autoload.php';
 use PhpAmqpLib\Connection\AMQPConnection;
-use PhpAmqpLip\Message\AMQPMessage;
-// create a connection to the server 
-$connection = new AMQPConnection('localhost', 80, 'guest', 'guest');
+use PhpAmqpLib\Message\AMQPMessage;
+
+// Create a connection to the server
+
+$connection = new AMQPConnection('localhost', 5672, 'guest', 'guest');
 $channel = $connection->channel();
 
-// create a channel and declare a queue for us to send to
+// Declare the queue
+
 $channel->queue_declare('hello', false, false, false, false);
+
 $msg = new AMQPMessage('Hello World!');
 $channel->basic_publish($msg, '', 'hello');
 
 echo " [x] Sent 'Hello World!'\n";
 
-// close the channel 
+// Close the channel and the connection
+
 $channel->close();
 $connection->close();
+
 ?>
+
 
